c9674b1a0f07051997d1aa2f742c867570a3c0d0
WilhamsGomes <wilhamsgoomes@gmail.com>
2022-11-21 19:20:35 -0300

    Adicionando sessão de experiência e projetos

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ var typed = new Typed('#typed', {
       cursorChar: '',
 });
 
-$('input').on('change', function() {
-  $('body').toggleClass('blue');
-});
+// $('input').on('change', function() {
+//   $('body').toggleClass('blue');
+// });
 
 function openNav(){
   document.getElementById('navBar-responsivo').style.display='flex'
@@ -16,4 +16,34 @@ function openNav(){
 
 function closeNav(){
   document.getElementById('navBar-responsivo').style.display='none'
+}
+
+function onActiveClass(nameId){
+
+  document.getElementById('etemb').classList.remove('activeExperience')
+  document.getElementById('redeCidada').classList.remove('activeExperience')
+  document.getElementById('singlePoint').classList.remove('activeExperience')
+
+  elemento = document.getElementById(nameId)
+  elemento.classList.add('activeExperience')
+
+  if(elemento.id == 'redeCidada'){
+    document.getElementById("titulo-experiencia").innerHTML="Monitor"
+    document.getElementById("periodo-experiencia").innerHTML="Maio de 2022 - Agosto de 2022"
+    document.getElementById("cargo-experiencia").innerHTML="Rede Cidadã/Start Recife by Accenture"
+  }
+
+  if(elemento.id == 'etemb'){
+    document.getElementById("titulo-experiencia").innerHTML="Desenvolvedor Back-end"
+    document.getElementById("periodo-experiencia").innerHTML="Junho de 2019 - Setembro de 2019"
+    document.getElementById("cargo-experiencia").innerHTML="ETE Miguel Batista"
+  }
+
+  if(elemento.id == 'singlePoint'){
+    document.getElementById("titulo-experiencia").innerHTML="Desenvolvedor Full Stack"
+    document.getElementById("periodo-experiencia").innerHTML="Abril de 2022 - Outubro de 2022"
+    document.getElementById("cargo-experiencia").innerHTML="Single Point Technology"
+  }
+
+
 }
