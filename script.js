8c3702c27325b6fab9256f5704f57bcd3c6c9a1c
Wilhams Gomes <71606861+WilhamsGomes@users.noreply.github.com>
2023-10-20 20:29:35 -0300

    feat: update

--- script.js
+++ script.js
@@ -27,9 +27,9 @@ function onActiveClass(nameId){
 
   if(elemento.id == 'redeCidada'){
     document.getElementById("titulo-experiencia").innerHTML="Monitor"
-    document.getElementById("periodo-experiencia").innerHTML="Maio de 2022 - Agosto de 2022"
+    document.getElementById("periodo-experiencia").innerHTML="Maio de 2022 - Agosto de 2022 | Abril de 2023 - Agosto de 2023"
     document.getElementById("cargo-experiencia").innerHTML="Rede Cidadã/Start Recife by Accenture"
-    document.getElementById("text-experiencia").innerHTML="Atuei como monitor na formação de jovens na Capacitação Full Stack, oferecida pela Rede Cidadã/Start Recife em parceria com a Accenture. Minha função era auxiliar os alunos a compreenderem os conteúdos do curso, entre eles Lógica de Programação, MySQL, HTML, CSS, JavaScript, React.Js e Node.js"
+    document.getElementById("text-experiencia").innerHTML="Desempenhei o papel de monitor na Capacitação Full Stack, um programa oferecido pela Rede Cidadã/Start Recife em parceria com a Accenture, voltado para a formação de jovens. Minha responsabilidade era auxiliar os alunos na compreensão dos conteúdos do curso, que incluíam Lógica de Programação, MySQL, HTML, CSS, JavaScript, React.Js e Node.js. Este trabalho voluntário não só me proporcionou a oportunidade de contribuir para a formação de futuros profissionais da área de tecnologia, mas também enriqueceu meu próprio aprendizado e desenvolvimento pessoal. Foi uma experiência gratificante que me permitiu aplicar e expandir meus conhecimentos enquanto ajudava outros a fazer o mesmo."
   }
 
   if(elemento.id == 'etemb'){
@@ -44,7 +44,7 @@ function onActiveClass(nameId){
     document.getElementById("titulo-experiencia").innerHTML="Desenvolvedor Full-Stack"
     document.getElementById("periodo-experiencia").innerHTML="Abril de 2022 - Outubro de 2022"
     document.getElementById("cargo-experiencia").innerHTML="Single Point Technology"
-    document.getElementById("text-experiencia").innerHTML="Atuei como Desenvolvedor Full Stack na empresa Single Point Technology. Utilizei JavaScript, Jquery, Java, Groovy e Oracle SQL para o desenvolvimento de novas funcionalidades em sistemas CRM, resolução de bugs e criação de regras de negócios a partir da solicitação de clientes."
+    document.getElementById("text-experiencia").innerHTML="Desempenhei o papel de Desenvolvedor Full Stack na Single Point Technology, onde utilizei uma variedade de tecnologias, incluindo JavaScript, Jquery, Java, Groovy e Oracle SQL. Essas ferramentas foram fundamentais para o desenvolvimento de novas funcionalidades em sistemas CRM, a resolução de bugs e a criação de regras de negócios personalizadas para atender às solicitações dos clientes. Além disso, tive a oportunidade de participar do desenvolvimento de uma plataforma que ajudou a empresa a cumprir as diretrizes da LGPD. Também contribuí para um projeto que envolvia a criação de sistemas de controle de estoque para grandes empresas farmacêuticas. Essas experiências ampliaram minha compreensão dos desafios e soluções no campo do desenvolvimento de software."
   }
 
   if(elemento.id == 'gizmidia'){
@@ -55,10 +55,10 @@ function onActiveClass(nameId){
   }
 
   if(elemento.id == 'gdstec'){
-    document.getElementById("titulo-experiencia").innerHTML="Desenvolvedor Front-End"
+    document.getElementById("titulo-experiencia").innerHTML="Desenvolvedor Full-Stack"
     document.getElementById("periodo-experiencia").innerHTML="Dezembro de 2022 - Até o momento"
     document.getElementById("cargo-experiencia").innerHTML="GDS Tec"
-    document.getElementById("text-experiencia").innerHTML="Atuo como desenvolvedor Front-End na empresa GDS Tec, contribuindo com a produção de novos produtos para a GDS ou para a F12 - empresa parceira. Atualmente utilizo React.js, Next.js e Vue.js para o desenvolvimento de projetos."
+    document.getElementById("text-experiencia").innerHTML="Atualmente, desempenho o papel de Desenvolvedor Full-Stack na GDS Tec, onde tenho a oportunidade de contribuir para a criação de novos produtos, tanto para a própria GDS quanto para a F12, uma empresa parceira. Minha experiência abrange o uso de várias tecnologias, incluindo React.js, Next.js, Vue.js, Python, .NET e NestJS, que são essenciais para o desenvolvimento dos projetos em que estou envolvido. Além disso, tive a chance de participar do desenvolvimento de dashboards, uma plataforma de apostas online e SASS. Esses produtos são comercializados externamente, ampliando ainda mais o impacto do meu trabalho."
   }
 
 
